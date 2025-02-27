14daf5fc144b7173d51f88f50680a6d1882bf7c2
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-02-28 02:41:00 +0600

    added new functionality and added here and changed background color

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-alert('shihab')
+document.getElementById('color-purple').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'purple';
+});
+document.getElementById('color-blue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+});
+document.getElementById('color-skyBlue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'skyBlue';
+});
+document.getElementById('color-green').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
+document.getElementById('color-yellow').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'yellow';
+});
+
+document.getElementById('color-orange').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'orange';
+});
+document.getElementById('color-red').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+});
